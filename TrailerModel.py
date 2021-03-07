589fdf720808c9c6be67aece6fb04713748bd83f
FarouQQQ <omarf.essam@gmail.com>
2021-03-07 23:06:54 +0200

    Add basic strucure for core functions to be implemented

---
+++ TrailerModel.py
@@ -0,0 +1,15 @@
+'''
+    File name: TrailerModel.py
+    Author: Omar Elfarouk
+    Contributors : Omar Tag
+    Date created: 7/3/2021
+    Date last modified: 7/3/2021
+    Python Version: 2.7
+'''
+'''
+    functions :
+    Init : initialize model configurations (calls SetParameters)
+    Update : Cyclic Update of trailer model
+    Reset : Resets Model states and returns to initial states
+    SetParameters : Sets values for trailer configuration parameters
+'''
